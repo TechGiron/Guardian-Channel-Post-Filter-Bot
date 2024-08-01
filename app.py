0e2cf85166f1db45584594d479db07e2edc81b8d
Rahul <111085537+TechGiron@users.noreply.github.com>
2024-08-01 19:05:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,9 +5,6 @@ app = Flask(__name__)
 def hello_world():
     return 'Bharat'
 
-def run_bot():
-    print("Bot Started 👍 Powered By @unicornguardian")
-    Bot().run()
 
 if __name__ == "__main__":
-    app.run
+    app.run()
